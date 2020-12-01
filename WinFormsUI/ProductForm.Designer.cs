b9073d7916428639baf3236fc779b41d29dbe181
Engin Demiroğ <engindemirog@gmail.com>
2020-12-01 12:13:57 +0300

    ui

--- WinFormsUI/ProductForm.Designer.cs
+++ WinFormsUI/ProductForm.Designer.cs
@@ -29,6 +29,17 @@
         private void InitializeComponent()
         {
             this.dgrwProducts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxProductName = new System.Windows.Forms.TextBox();
+            this.tbxCategoryId = new System.Windows.Forms.TextBox();
+            this.tbxUnitPrice = new System.Windows.Forms.TextBox();
+            this.tbxUnitsInStock = new System.Windows.Forms.TextBox();
+            this.tbxQuantityPerUnit = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgrwProducts)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,22 +54,135 @@
             this.dgrwProducts.TabIndex = 0;
             this.dgrwProducts.Text = "dataGridView1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 349);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kategori : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 440);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fiyat : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 474);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Stok Adedi : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 515);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Açıklama : ";
+            // 
+            // tbxProductName
+            // 
+            this.tbxProductName.Location = new System.Drawing.Point(193, 342);
+            this.tbxProductName.Name = "tbxProductName";
+            this.tbxProductName.Size = new System.Drawing.Size(251, 27);
+            this.tbxProductName.TabIndex = 6;
+            // 
+            // tbxCategoryId
+            // 
+            this.tbxCategoryId.Location = new System.Drawing.Point(193, 379);
+            this.tbxCategoryId.Name = "tbxCategoryId";
+            this.tbxCategoryId.Size = new System.Drawing.Size(251, 27);
+            this.tbxCategoryId.TabIndex = 7;
+            // 
+            // tbxUnitPrice
+            // 
+            this.tbxUnitPrice.Location = new System.Drawing.Point(193, 425);
+            this.tbxUnitPrice.Name = "tbxUnitPrice";
+            this.tbxUnitPrice.Size = new System.Drawing.Size(251, 27);
+            this.tbxUnitPrice.TabIndex = 8;
+            // 
+            // tbxUnitsInStock
+            // 
+            this.tbxUnitsInStock.Location = new System.Drawing.Point(193, 474);
+            this.tbxUnitsInStock.Name = "tbxUnitsInStock";
+            this.tbxUnitsInStock.Size = new System.Drawing.Size(251, 27);
+            this.tbxUnitsInStock.TabIndex = 9;
+            // 
+            // tbxQuantityPerUnit
+            // 
+            this.tbxQuantityPerUnit.Location = new System.Drawing.Point(193, 515);
+            this.tbxQuantityPerUnit.Name = "tbxQuantityPerUnit";
+            this.tbxQuantityPerUnit.Size = new System.Drawing.Size(251, 27);
+            this.tbxQuantityPerUnit.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(350, 562);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
             // ProductForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1190, 645);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbxQuantityPerUnit);
+            this.Controls.Add(this.tbxUnitsInStock);
+            this.Controls.Add(this.tbxUnitPrice);
+            this.Controls.Add(this.tbxCategoryId);
+            this.Controls.Add(this.tbxProductName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgrwProducts);
             this.Name = "ProductForm";
             this.Text = "ProductForm";
             this.Load += new System.EventHandler(this.ProductForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgrwProducts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgrwProducts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxProductName;
+        private System.Windows.Forms.TextBox tbxCategoryId;
+        private System.Windows.Forms.TextBox tbxUnitPrice;
+        private System.Windows.Forms.TextBox tbxUnitsInStock;
+        private System.Windows.Forms.TextBox tbxQuantityPerUnit;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
--- WinFormsUI/ProductForm.cs
+++ WinFormsUI/ProductForm.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,7 +22,23 @@ namespace WinFormsUI
         IProductService _productService = new ProductManager(new EfProductDal());
         private void ProductForm_Load(object sender, EventArgs e)
         {
+            dgrwProducts.DataSource = null;
             dgrwProducts.DataSource = _productService.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var product = new Product
+            {
+                CategoryId = Convert.ToInt32(tbxCategoryId.Text),
+                ProductName = tbxProductName.Text,
+                QuantityPerUnit = tbxQuantityPerUnit.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                UnitsInStock = Convert.ToInt16(tbxUnitsInStock.Text)
+            };
+
+            _productService.Add(product);
+            ProductForm_Load(sender,e);
         }
     }
 }
