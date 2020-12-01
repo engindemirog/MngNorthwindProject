875c57cc1ddc8b1afb73013346c6d1b988a8a2bc
Engin Demiroğ <engindemirog@gmail.com>
2020-12-01 11:48:00 +0300

    UI

---
+++ WinFormsUI/ProductForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WinFormsUI
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgrwProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrwProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgrwProducts
+            // 
+            this.dgrwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrwProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgrwProducts.Location = new System.Drawing.Point(0, 0);
+            this.dgrwProducts.Name = "dgrwProducts";
+            this.dgrwProducts.RowHeadersWidth = 51;
+            this.dgrwProducts.Size = new System.Drawing.Size(1190, 297);
+            this.dgrwProducts.TabIndex = 0;
+            this.dgrwProducts.Text = "dataGridView1";
+            // 
+            // ProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 645);
+            this.Controls.Add(this.dgrwProducts);
+            this.Name = "ProductForm";
+            this.Text = "ProductForm";
+            this.Load += new System.EventHandler(this.ProductForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgrwProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgrwProducts;
+    }
+}
---
+++ WinFormsUI/ProductForm.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsUI
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+
+        IProductService _productService = new ProductManager(new EfProductDal());
+        private void ProductForm_Load(object sender, EventArgs e)
+        {
+            dgrwProducts.DataSource = _productService.GetAll();
+        }
+    }
+}
